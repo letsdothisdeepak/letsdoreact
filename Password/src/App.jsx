0b4d153435c6e685b017d41c9eb71169b2552ec7
Deepak <workwithmedeepak@gmail.com>
2024-12-29 14:21:28 +0530

    Password generator project

--- Password/src/App.jsx
+++ Password/src/App.jsx
@@ -1,18 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useCallback, useEffect } from 'react'
+
 import './App.css'
 
-function App() {
+
+function App(){
   const [length, setLength] = useState(8)
   const [numberChecked, setNumberChecked] = useState(false)
   const [charChecked, setCharChecked] = useState(false)
-  const [password,setPassword] = useState()
+  const [password,setPassword] = useState("")
 
-  const lengthChange=()=>{
-    setLength(length)
-  }
+  const passwordGenerator= useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    
+    if(numberChecked){
+      str= str + "0123456789"
+    }
+    if(charChecked){
+      str=str + "!@#$%^&*()_+"
+    }
 
+    for (let i = 1; i <=length; i++) {
+     let char= Math.floor(Math.random()*str.length + 1)
+       pass= pass+str.charAt(char)
+
+      
+    }
+    setPassword(pass)
+
+  },[length,numberChecked,charChecked,setPassword])
+
+  useEffect(()=>{
+    passwordGenerator()},[length,numberChecked,charChecked,passwordGenerator]
+  )
+
+  
 
 
   return (
@@ -21,15 +43,39 @@ function App() {
         <h4>Password Generator</h4>
         <div>
         <label htmlFor="text"></label>
-        <input type="text" name="text" id="password" placeholder='your-generated-password-is-here'/>
+        <input 
+        type="text" 
+        value={password} 
+        name="text" 
+        id="password" 
+        placeholder='your-generated-password-is-here'
+        readOnly/>
         <button>copy</button>
         </div>
         <div class='details'>
         
-        <input type="range" id="range" min="8" max="100" />
+        <input type="range"
+         id="range" 
+         min="5" max="100"
+         value={length}
+         onChange={(e)=>{setLength(e.target.value)}} />
         <span>Length({length})</span>
-        <span><input type="checkbox" name="Numbers" id="numbers" />Numbers</span>
-        <span><input type="checkbox" name="Characters" id="characters" />Characters</span>
+        <span><input 
+                type="checkbox" 
+                name="Numbers"
+                id="numbers"
+                onChange={()=>{setNumberChecked((last)=> !last)
+
+                }} />Numbers
+        </span>
+        <span><input 
+                type="checkbox"
+                name="Characters" 
+                id="characters"
+                onChange={()=>{setCharChecked((last)=> !last)
+
+                }} />Characters
+        </span>
         </div>
         
      </div>
